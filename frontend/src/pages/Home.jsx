48b4b16e1f9c205308f5f03f513c28a0ce1af386
JainilModi <jainilrmodi2013@gmail.com>
2024-02-12 22:53:28 +0530

    added signin button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,30 @@
-import { Button } from "../components/Button"
+import { Button } from "../components/Button";
 import { useNavigate } from "react-router-dom";
 
-export const Home =() => {
-    const navigate=useNavigate();
-    return (
-        <div>
-            <Button label={"Go to signup"} onPress={async () => {navigate("/signup")}}/>
-        </div>
-    )
-}
+export const Home = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <br>
+        <br></br>
+      </br>
+      <Button
+        label={"Go to signup"}
+        onPress={async () => {
+          navigate("/signup");
+        }}
+      />
+      <div>
+        <br>
+          <br></br>
+        </br>
+      </div>
+      <Button
+        label={"Go to signin"}
+        onPress={async () => {
+          navigate("/signin");
+        }}
+      />
+    </div>
+  );
+};
